40dfe4fcb29f1be5817b4bde9394f665bb5342e9
AndreDG88 <andre.soares.designer@gmail.com>
2025-02-12 22:44:38 -0300

    Ajuste do menu header para o modo responsivo

--- src/components/Header/header-index.tsx
+++ src/components/Header/header-index.tsx
@@ -1,17 +1,27 @@
 //Arquivo de criação e configuração do cabeçalho da página.
 import { Link } from 'react-router-dom'
-import { Headerbar, Links, LinkItem, CartButton } from './header-styles'
+import {
+  Headerbar,
+  Links,
+  LinkItem,
+  CartButton,
+  Hamburguer,
+  HeaderRow,
+  NavMobile
+} from './header-styles'
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
 import { open } from '../../store/reducers/cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store/store-index'
+import { useState } from 'react'
 
 //Const principal do Cabeçalho.
 const Header = () => {
   //const para função de abrir a área do carrinho.
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart) //Para ler as propriedades do reducer e contar os itens do carrinho
+  const [isMenuOpen, setIsMenuOpen] = useState(false) //Para fazer a função da abertura do menu mobile.
 
   const openCart = () => {
     dispatch(open())
@@ -19,33 +29,58 @@ const Header = () => {
 
   return (
     <Headerbar>
-      <div>
-        {/* logo */}
-        <Link to="/">
-          <img src={logo} alt="EPLAY" />
-        </Link>
+      <HeaderRow>
+        <div>
+          {/* Menu para Mobile - aparece apenas na visão smartphone */}
+          <Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburguer>
 
-        {/* links de menu */}
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categories">Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
+          {/* logo */}
+          <Link to="/">
+            <img src={logo} alt="EPLAY" />
+          </Link>
 
-      {/* carrinho */}
-      <CartButton onClick={openCart}>
-        {items.length} - produto(s)
-        <img src={carrinho} alt="carrinho" />
-      </CartButton>
+          {/* links de menu */}
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link to="/categories">Categorias</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Novidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Promoções</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+
+        {/* carrinho */}
+        <CartButton onClick={openCart}>
+          {items.length}
+          <span> - produto(s)</span>
+          <img src={carrinho} alt="carrinho" />
+        </CartButton>
+      </HeaderRow>
+
+      {/* Menu para Mobile - aparece apenas na visão smartphone */}
+      <NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link to="/categories">Categorias</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </Headerbar>
   )
 }
--- src/components/Header/header-styles.ts
+++ src/components/Header/header-styles.ts
@@ -2,39 +2,70 @@
 import styled from 'styled-components'
 import { breakpoints, cores } from '../../global-styles'
 
+//Configuração CSS dos links do menu.
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 //configuração CSS da barra de fundo.
 export const Headerbar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 
   a {
     color: ${cores.branco};
     text-decoration: none;
     font-weight: bold;
   }
+`
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-  div {
+  > div {
     display: flex;
     align-items: center;
-  }
 
-  @media (max-width: ${breakpoints.tablet}) {
-    display: none;
+    @media (max-width: ${breakpoints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+
+      ${Links} {
+        display: none;
+      }
+    }
   }
 `
 
-//Configuração CSS dos links do menu.
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+export const NavMobile = styled.nav`
+  display: none;
+
+  &.is-open {
+    display: block;
+  }
 `
+
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 
 //Configuração CSS do Carrinho.
@@ -43,5 +74,28 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+  span {
+    @media (max-width: ${breakpoints.tablet}) {
+      display: none;
+    }
+  }
+`
+
+//Configuração CSS para menu mobile
+export const Hamburguer = styled.div`
+  width: 32px;
+
+  span {
+    height: 2px;
+    display: block;
+    width: 100%;
+    background-color: ${cores.branco};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakpoints.tablet}) {
+    display: none;
   }
 `
